0f6a3b3bd52ff653e31c5ead8f7733e5c88029d9
Moosan <washer2016@outlook.jp>
2020-02-06 01:04:51 +0900

    Add Project

---
+++ Assets/iOSExternalInput/Scripts/ExternalInputController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void ExternalInputDidReceiveKey(KeyCode receivedKey);
+public class ExternalInputController : MonoBehaviour {
+
+	public static ExternalInputController instance = null;
+
+	public event ExternalInputDidReceiveKey OnExternalInputDidReceiveKey;
+
+	void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		} else if(instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void Start()
+	{
+		ExternalInputInterface.SetupExternalInput();
+	}
+
+	public void DidReceiveKeystroke(string receivedKeystroke)
+	{
+		KeyCode receivedKeyCode = (KeyCode)System.Enum.Parse(typeof(KeyCode), receivedKeystroke);
+
+		if(OnExternalInputDidReceiveKey != null)
+			OnExternalInputDidReceiveKey(receivedKeyCode);
+	}
+}
